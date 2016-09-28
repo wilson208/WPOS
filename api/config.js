30db95ef0c873cbb34d03ee1dd187fda8922e842
Wilson McCoubrey <wilson@puppet.com>
2016-09-29 00:55:29 +0100

    Another change to travis

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'asecretkeythatwillbechangedifeverinproduction',
     'database': 'mongodb://testing:testing@olympia.modulusmongo.net:27017/owEdid4e',
-    'travisdatabase': 'mongodb://travis:test@127.0.0.1:27017/test_db'
+    'travisdatabase': 'mongodb://travis:test@127.0.0.1:27017/mydb_test'
 };
