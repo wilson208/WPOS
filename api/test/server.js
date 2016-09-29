8915446e8569dd635719bb13592b9300baaa56e8
Wilson McCoubrey <wilson@puppet.com>
2016-09-29 01:18:11 +0100

    Changes to testing

--- api/test/server.js
+++ api/test/server.js
@@ -19,6 +19,12 @@ describe('API Tests', function() {
     before(function () {
         server = app.listen(8000);
         log.log("Server Started");
+
+        models.User.remove({}, function(err){
+            expect(err).to.be.null;
+        });
+        log.log("Existing users deleted");
+
         var usr = new models.User({
             username: "testuser",
             password: "testpass"
@@ -29,7 +35,9 @@ describe('API Tests', function() {
     });
 
     after(function () {
-        models.User.findOneAndRemove({username: "testuser"});
+        models.User.remove({}, function(err){
+            expect(err).to.be.null;
+        });
         log.log("Removed test user");
         server.close();
         log.log("Server Stopped");
@@ -118,18 +126,70 @@ describe('API Tests', function() {
             done();
         });
 
-        describe('/Users Endpoint', function(done){
+        describe('/Users Endpoint', function(){
+            it('Should return status 200 and one or more users', function(done){
+                chai.request(app)
+                    .get('/users')
+                    .send({ token: token })
+                    .end(function (err, res) {
+                        log.log(JSON.stringify(res.body));
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a("array");
+                        expect(res.body.length).to.be.gte(1);
+                        done();
+                    });
+            })
 
+            it('User Count Should Increase After Adding New User', function(done){
+                //Do the count
+                chai.request(app)
+                    .get('/users')
+                    .send({ token: token })
+                    .end(function (err, res) {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a("array");
+                        expect(res.body.length).to.be.gte(1);
+                        var originalCount = res.body.length;
+
+                        //Add a new user
+                        var usr = new models.User({
+                            username: "anewuser",
+                            password: "anewpass"
+                        });
+                        usr.save();
+
+                        //Recount and ensure only 1 is added
+                        chai.request(app)
+                            .get('/users')
+                            .send({ token: token })
+                            .end(function (err, res) {
+                                expect(err).to.be.null;
+                                expect(res).to.have.status(200);
+                                expect(res.body).to.be.a("array");
+                                expect(res.body.length).to.be.eql(originalCount + 1);
+
+                                done();
+                            });
+                    });
+            })
         });
 
         describe('/user/me Endpoint', function(done){
-
+            it('Should return status 200 and correct details', function(){
+                chai.request(app)
+                    .get('/user/me')
+                    .send({ token: token })
+                    .end(function (err, res) {
+                        expect(err).to.be.null;
+                        expect(res).to.have.status(200);
+                        expect(res.body).to.be.a("array");
+                        expect(res.body.username).to.be.eql('testuser');
+                        expect(res.body.password).to.be.eql('testpass');
+                        done();
+                    });
+            });
         });
-
     });
-
-    describe('Unauthenticated Endpoints', function(){
-
-    });
-
 });
